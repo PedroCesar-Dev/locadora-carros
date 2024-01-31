7baa788fcd70183cfcfa63bf09a9c80b6e3baf02
Pedro Cesar <119058413+PedroCesar-Dev@users.noreply.github.com>
2024-01-31 17:08:25 -0300

    Add files via upload

---
+++ locadora/Program.cs
@@ -0,0 +1,56 @@
+﻿//Variaveis
+using locadora;
+
+decimal precoInicial, precoHora = 0;
+
+//Iniciar sistema
+
+Console.WriteLine("Seja Bem vindos ao estacionamento");
+Console.WriteLine("---------------------------------");
+
+Console.WriteLine("Digite o preço inicial: ");
+precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Agora digite o preço por hora: ");
+precoHora = Convert.ToDecimal(Console.ReadLine());
+
+//Estancia a classe estacionamento
+
+estacionamento es = new estacionamento(precoInicial, precoHora);
+
+//loop
+
+bool menu = true;
+
+while (menu)
+{
+    Console.Clear();
+    Console.WriteLine("Escolha uma opção");
+    Console.WriteLine("1. - Adicionar veículo");
+    Console.WriteLine("2. - Remover");
+    Console.WriteLine("3. - Listar veículo");
+    Console.WriteLine("4. - Sair do sistema");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            es.AdicionarVeiculo();
+            break;
+        case "2":
+            es.RemoverVeiculo();
+            break;
+        case "3":
+            es.listarVeiculo();
+            break;
+        case "4":
+            menu = false;
+            break;
+        default:
+            Console.WriteLine("Digitar uma opção válida");
+            break;
+    }
+    Console.WriteLine("Digite uma tecla para continuar:");
+    Console.ReadLine();
+    
+}
+Console.WriteLine("O programa encerrou");
---
+++ locadora/estacionamento.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace locadora
+{
+    internal class estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public estacionamento( decimal precoInicial, decimal precoHora) 
+        {
+            this.precoInicial = precoInicial;
+            this.precoHora = precoHora;
+        }
+        
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo:");
+            string placa = Console.ReadLine();
+            placa = placa.ToUpper();
+
+            if (veiculos.Any(x => x.Equals(placa.ToUpper())))
+            {
+                Console.WriteLine("Já existe um veículo com essa placa");
+            }
+            else
+            {
+                veiculos.Add(placa);
+                Console.WriteLine("Veículo cadastrado com sucesso");
+            }
+        }
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo");
+            string placa = Console.ReadLine();
+            placa = placa.ToUpper();
+
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Informe a quantidade de horas que o veículo permaneceu no pátio: ");
+                int horas = Convert.ToInt32(Console.ReadLine());
+                decimal precoTotal = precoInicial + precoHora * horas;
+                veiculos.Remove(placa);
+                Console.WriteLine($"O veículo de placa: {placa} foi removido e deve pagar {precoTotal}");
+            }
+            else
+            {
+                Console.WriteLine("O veículo não está em nosso pátio. ");
+            }
+        }
+        public void listarVeiculo()
+        { 
+            if (veiculos.Any())
+            {
+                Console.Write("Os veículos estacionados são:");
+                foreach (var veiculo in veiculos)
+                {
+                    Console.WriteLine(veiculo);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos no pátio");
+            }
+        }
+
+    }
+}
